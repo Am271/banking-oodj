4be3dfb4fc8a60a8836b1df71b904f4b3d092eaf
Amitash <amitu895@gmail.com>
2021-11-24 21:55:48 +0530

    Redefined Person class

--- Person.java
+++ Person.java
@@ -1,9 +1,17 @@
 public class Person {
-  private String name;
-  private long phoneNo;
-  private String email;
-  private String address;
-  private String ssn;
-  private String dob;
-  private String pno;
+    private String name;
+    private String phone_no;
+    private String email;
+    private String address;
+    private String ssn; // Social Security number
+    private String dob;
+
+    public Person(String name, String phone_no, String email, String address, String ssn, String dob) {
+        this.name = name;
+        this.phone_no = phone_no;
+        this.email = email;
+        this.address = address;
+        this.ssn = ssn;
+        this.dob = dob;
+    }
 }
