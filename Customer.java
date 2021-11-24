5221d04b33d50cb33c79a6884974b55c5127d3db
Amitash <amitu895@gmail.com>
2021-11-24 21:56:24 +0530

    Redefined Customer class

--- Customer.java
+++ Customer.java
@@ -1,90 +1,28 @@
 public class Customer extends Person {
-   private String name;
-   private String phone_no;
-   private String email;
-   private String address;
-   private String ssn;
-   private String dob;
-   private String pan;
-   private String acc_no;
-   private String ac_open_date;
-   private String home_br;
+    private String pan_no;
+    private String acc_no;
+    private String acc_bal;
+    private String acc_open_dt;
+    private String home_br;
 
-   public Customer(String name,
-   String phone_no,
-   String email,
-   String address,
-   String ssn,
-   String dob,
-   String pan,
-   String acc_no,
-   String ac_open_date,
-   String home_br) {
-      this.name= name;
-      this.phone_no = phone_no;
-      this.email = email;
-      this.address = address;
-      this.ssn = ssn;
-      this.dob = dob;
-      this.pan = pan;
-      this.acc_no = acc_no;
-      this.ac_open_date = ac_open_date;
-      this.home_br = home_br;
-   }
+    public Customer(String name, String phone_no, String email, String address, String ssn, String dob, String pan_no, String acc_no, String acc_bal, String acc_open_dt, String home_br) {
+        super(name, phone_no, email, address, ssn, dob);
+        this.pan_no = pan_no;
+        this.acc_no = acc_no;
+        this.acc_bal = acc_bal;
+        this.acc_open_dt = acc_open_dt;
+        this.home_br = home_br;
+    }
 
-   public Customer(String name,
-   String phone_no,
-   String email,
-   String address,
-   String acc_no,
-   String ac_open_date,
-   String home_br) {
-      this.name= name;
-      this.phone_no = phone_no;
-      this.email = email;
-      this.address = address;
-      this.acc_no = acc_no;
-      this.ac_open_date = ac_open_date;
-      this.home_br = home_br;
-   }
+    public double CheckBalance() {
+        return this.acc_bal;
+    }
 
-   public String getName() {
-      return this.name;
-   }
+    public String DisplayDetails() {
+        // Code yet to be implemented
+    }
 
-   public String getPhone_no() {
-      return this.phone_no;
-   }
-   
-   public String getEmail() {
-      return this.email;
-   }
+    public void UpdateDetails() {
 
-   public String getAddr() {
-      return this.address;
-   }
-
-   public String getAcc_no() {
-      return this.acc_no;
-   }
-
-   public String getAcc_open_date() {
-      return this.acc_open_date;
-   }
-
-   public String getHome_br() {
-      return this.home_br;
-   }
-
-   public void setPhone_no(String phone_no) {
-      this.phone_no = phone_no;
-   }
-
-   public void setEmail(String email) {
-      this.email = email;
-   }
-
-   public void setAddr(String address) {
-      this.address = address;
-   }
+    }
 }
